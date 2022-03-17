f759df0216717d2c3ff06c08e50f9b41e9ba8fc9
MUKILTEO\1710219 <1710219@mulkilteo.wednet.edu>
2022-03-17 12:30:29 -0700

    Saving

---
+++ Assets/Adrian KeepOut/Scripts/NPCMOVEMENTS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NPCMOVEMENTS : MonoBehaviour
+{
+    Rigidbody npc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        npc = GetComponent<Rigidbody>();
+        
+    }
+
+   
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (true)
+        {
+            npc.AddForce(0, 1, 0);
+        }
+        
+    }
+}
