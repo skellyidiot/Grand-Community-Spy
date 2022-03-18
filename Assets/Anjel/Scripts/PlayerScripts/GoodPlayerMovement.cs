886b1b4182248ceb29a09f1964dc81e839d79ee4
MUKILTEO\1553223 <aglez0657@gmail.com>
2022-03-18 12:24:34 -0700

    working on new topdown tutorial

---
+++ Assets/Anjel/Scripts/PlayerScripts/GoodPlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodPlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody rb;
+
+    Vector3 moveInput;
+    Vector3 moveVelocity;
+
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+
+
+    }
+}
