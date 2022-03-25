5f1788b093691c2a5087db33346abc3470697c81
MUKILTEO\1553223 <aglez0657@gmail.com>
2022-03-25 11:47:24 -0700

    please dont merge conflict pls

--- Assets/Anjel/Scripts/PlayerScripts/CarController.cs
+++ Assets/Anjel/Scripts/PlayerScripts/CarController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public float driftFactor = 0.95f;
     public float accelerationFactor = 30.0f;
-    public float turnFactor = 35f;
+    public float turnFactor = 3.5f;
+    public float maxSpeed = 40;
 
     float accelerationInput = 0;
     float steeringInput = 0;
     float rotationAngle = 0;
+
+    float velocityVsUP = 0;
 
     Rigidbody2D carRB2;
 
@@ -28,15 +32,29 @@ public class CarController : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyEngineForce();
-        
-        if(Mathf.Abs(carRB2.velocity.x) + Mathf.Abs(carRB2.velocity.y) >= 1)
-        {
-            ApplySteering();
-        }
+        KillOrthogonalVelocity();
+        ApplySteering();
+    //    if(Mathf.Abs(carRB2.velocity.x) + Mathf.Abs(carRB2.velocity.y) >= 1)
+    //    {
+    //        ApplySteering();
+    //    }
     }
 
     void ApplyEngineForce()
     {
+        velocityVsUP = Vector2.Dot(transform.up, carRB2.velocity);
+
+        if (velocityVsUP > maxSpeed && accelerationInput > 0) return;
+        if (velocityVsUP < -maxSpeed * 0.5f && accelerationInput < 0) return;
+
+        if (carRB2.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0) return;
+
+        if (accelerationInput == 0)
+        {
+            carRB2.drag = Mathf.Lerp(carRB2.drag, 3.0f, Time.fixedDeltaTime * 3);
+        }
+        else carRB2.drag = 0f;
+
         Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
 
         carRB2.AddForce(engineForceVector, ForceMode2D.Force);
@@ -44,8 +62,22 @@ public class CarController : MonoBehaviour
 
     void ApplySteering()
     {
-        carRB2.angularVelocity -= steeringInput * turnFactor;
+        float minSpeedBeforeAllowTurning = (carRB2.velocity.magnitude / 8);
+        minSpeedBeforeAllowTurning = Mathf.Clamp01(minSpeedBeforeAllowTurning);
 
+        //carRB2.angularVelocity -= steeringInput * turnFactor;
+
+        rotationAngle -= steeringInput * turnFactor;
+
+        carRB2.MoveRotation(rotationAngle);
+    }
+    
+    void KillOrthogonalVelocity()
+    {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(carRB2.velocity, transform.up);
+        Vector2 rightVelocity = transform.right * Vector2.Dot(carRB2.velocity, transform.right);
+
+        carRB2.velocity = forwardVelocity + rightVelocity * driftFactor;
     }
 
     public void SetInputVector(Vector2 inputVector)
