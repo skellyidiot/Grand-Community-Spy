eca39572e93fbfd4e94d89805ee99f83b14f2d6d
E:\VGD\ILEIA <ileia.rezaei2005@gmail.com>
2022-03-29 13:35:38 -0700

    last push for 3/29

---
+++ Assets/Ileia R/Scripts/FOV/FieldOfView.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        Vector3 origin = Vector3.zero;
+        float fov = 90f;
+        int rayCount = 50;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 50f;
+
+        Vector3[] verticies = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[verticies.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        verticies[0] = origin;
+
+        int VertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i<=rayCount; i++)
+        {
+            Vector3 vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance);
+
+            if (raycastHit2D.collider == null)
+            {
+                //No hit
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+                //Hit object
+                vertex = raycastHit2D.point;
+            }
+
+            verticies[VertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = VertexIndex - 1;
+                triangles[triangleIndex + 2] = VertexIndex;
+
+                triangleIndex += 3;
+            }
+            VertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = verticies;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+}
