5a55c59d5ae2b52d07930f620d59c0d17386a65e
E:\VGD\ILEIA <ileia.rezaei2005@gmail.com>
2022-04-13 13:41:11 -0700

    tutorial level

---
+++ Assets/Ileia R/Scripts/Tutorial Level/CarTutorialText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTutorialText : MonoBehaviour
+{
+    public bool isInCar;
+    public GameObject thing;
+
+    private void Start()
+    {
+        isInCar = false;
+    }
+    private void Update()
+    {
+        isInCar = Car.isDriving;
+
+        if (isInCar) Destroy(thing);
+    }
+}
